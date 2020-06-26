4df80cc6567adc4299814587d9fa545c0838e088
Ha Tran <sherlockieee@has-macbook-pro.attlocal.net>
2020-06-26 10:41:08 -0700

    Updates

---
+++ precache-manifest.eafa63ca23f2c18acd85446eb933d27f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946573a3744386f202777d0c737798a8",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "03e9cadd194b7502e717",
+    "url": "/markdown-previewer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8c7e25cf59de2253519d",
+    "url": "/markdown-previewer/static/css/main.a86512f8.chunk.css"
+  },
+  {
+    "revision": "03e9cadd194b7502e717",
+    "url": "/markdown-previewer/static/js/2.25bd3692.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.25bd3692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7e25cf59de2253519d",
+    "url": "/markdown-previewer/static/js/main.d8fc63bf.chunk.js"
+  },
+  {
+    "revision": "2e760081d24f89f1b287",
+    "url": "/markdown-previewer/static/js/runtime-main.f3125055.js"
+  }
+]);
